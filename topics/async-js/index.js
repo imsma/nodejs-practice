84c9f10f2727f1da41eb47fee120b4bdee66a04f
SMA <sma@wizlinx.com>
2024-06-23 02:04:33 +0500

    Refactor asyncCall function to use async/await syntax

--- topics/async-js/index.js
+++ topics/async-js/index.js
@@ -1,23 +1,29 @@
 function resolveAfter2Seconds() {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve("resolved");
+      //   reject("Failure: The promise has failed!");
     }, 2000);
   });
 }
 
-async function asyncCall() {
-  console.log("calling");
+async function asyncCall1() {
+  console.log("calling asyncCall1");
   const result = await resolveAfter2Seconds();
-  console.log(result);
+  console.log("asyncCall1:::", result);
 }
 
-// function asyncCall() {
-//   console.log("calling");
+function asyncCall2() {
+  console.log("calling asyncCall2");
 
-//   resolveAfter2Seconds().then((res) => {
-//     console.log(res);
-//   });
-// }
+  resolveAfter2Seconds().then((res) => {
+    console.log("asyncCall2:::", res);
+  });
+}
 
-asyncCall();
+asyncCall1();
+asyncCall2();
+
+process.on("unhandledRejection", (reason, promise) => {
+  console.log(">>Unhandled Rejection at:", promise, "reason:", reason);
+});
